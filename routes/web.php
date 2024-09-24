dea43e0dda57491d186475261222baf6c9fce8f6
giorgiohendry <giorgiohendry@gmail.com>
2024-09-24 15:57:37 +0000

    Autenticación y autorización insegura

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//********************************************//
+// Mitigación de autenticación y autorización //
+//********************************************//
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/dashboard', function(){
+    // ...
+})->middleware('auth');
+
+//********************************************//
