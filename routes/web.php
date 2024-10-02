af5c3984405ee3f3bc4e9885f11cfeab157cb73d
giorgiohendry <giorgiohendry@gmail.com>
2024-10-02 23:05:34 +0000

    new changes

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,7 @@ Route::get('/', function () {
 //********************************************//
 
 Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
-    ->name('home');
-
-Route::get('/dashboard', function(){
-    // ...
-})
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])
     ->name('dashboard')
     ->middleware('auth');
 
