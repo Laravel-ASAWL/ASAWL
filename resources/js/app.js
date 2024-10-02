64df1b3db50d2c88485af4a21185251bdce52c6c
giorgiohendry <giorgiohendry@gmail.com>
2024-10-02 22:23:44 +0000

    new changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,3 @@
 import './bootstrap';
-import './vendor/bootstrap/4.5.3/js/bootstrap.bundle.min';
 import './vendor/jquery/3.7.1/js/jquery.min';
+import './vendor/bootstrap/4.5.3/js/bootstrap.bundle.min';
